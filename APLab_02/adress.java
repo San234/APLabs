821fb3fd529c41e579a674aa7390f4145820e35c
San234 <allen525@naver.com>
2016-09-16 10:40:11 -0700

    Added Files

--- APLab_02/adress.java
+++ APLab_02/adress.java
@@ -4,25 +4,23 @@ public class adress
 {
 	public static void main(String[]args)
 	{
-		System.out.print("|please use _ instead of Space|\n");
-		
 		Scanner user_input = new Scanner(System.in);
 		
 		String Full_name;
-		System.out.print("Enter the first name: ");
-		Full_name = user_input.next();
+		System.out.print("Enter the full name: ");
+		Full_name = user_input.nextLine();
 		
 		String Your_address;
 		System.out.print("Enter your adress: ");
-		Your_address = user_input.next();
+		Your_address = user_input.nextLine();
 		
 		String City_name;
 		System.out.print("Enter the name of city |including the initial of state|: ");
-		City_name = user_input.next();
+		City_name = user_input.nextLine();
 		
 		String Zip_code;
 		System.out.print("Enter the zip code: ");
-		Zip_code = user_input.next();
+		Zip_code = user_input.nextLine();
 		
 		String full_address;
 		full_address = Your_address + ", " + City_name + ", " + Zip_code;
---
+++ APLab_03/Guide.java
@@ -0,0 +1,47 @@
+//Scanner is imported...
+import java.util.Scanner;
+import java.util.Random;
+
+public class Guide
+{
+	public static void main(String[]args)
+	{
+		Scanner user_input = new Scanner(System.in);
+		
+		float score;
+		System.out.println("What is your grade? ");
+		score = user_input.nextFloat();
+		
+		
+		if(score > 100 || score < 0){
+			System.out.println("DO NOT LIE!!");
+			
+			System.out.print("Try Again? ");
+			String answer = user_input.next();
+			
+			if(answer == "y" || answer =="Y"){
+				
+			}
+			System.exit(0);
+		}
+		
+		char grade;
+		if(score >= 90){
+			grade = 'A';
+		}
+		else if(score >= 80){
+			grade = 'B';
+		}
+		else if(score >= 70){
+			grade = 'C';
+		}
+		else if(score >= 60){
+			grade = 'D';
+		}
+		else{
+			grade = 'F';
+		}
+		
+		System.out.println("Your Grade is " + grade);
+	}
+}
