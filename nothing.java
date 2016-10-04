ff48ada4c00fe5fa1b46105b6819ef958d6d7097
San234 <allen525@naver.com>
2016-10-04 12:15:19 -0700

    Edited

--- nothing.java
+++ nothing.java
@@ -16,27 +16,36 @@ public class nothing
 		int atk = rand.nextInt(64) + 36;
 		int def = rand.nextInt(48) + 32;
 		int exp = 0;
+		int level = 5;
 		
-		you.stat(user_name, hp, atk, def, exp);
+		you.stat(user_name, hp, atk, def, exp,level);
 	}
-	public static void stat(String user_name, int hp, int atk, int def, int exp)
+	public static void stat(String user_name, int hp, int atk, int def, int exp, int level)
 	{
 		Scanner up = new Scanner(System.in);
 		nothing you = new nothing();
+		
+		exp = exp + exp;
 		
 		String a = "atk";
 		String b = "hp";
 		String c = "def";
 		String d = "exp";
+		String e = "Level";
+			if(exp >= 200){
+			level = level + 1;
+			exp = exp - 200;
+		}
 		System.out.printf("-----------------" + "%2S" + "-------------\n", user_name);
 		System.out.printf("|" + "%8S" + " = " + "%5s" + "%8S" + " = " + "%6s" + "|\n", b, hp, a, atk);
 		System.out.printf("|" + "%8S" + " = " + "%5s" + "%8S" + " : " + "%6s" + "|\n", c, def, d, exp);
+		System.out.printf("|" + "%17S" + " : " + "%-13s" + "|\n",e , level);
 				
 		System.out.print("Would you go hunting?\n");
 		String hunt_ask = up.nextLine();
 		if(hunt_ask.equals("y") || hunt_ask.equals("yes"))
 		{
-			you.hunt(user_name, hp, atk, def, exp);
+			you.hunt(user_name, hp, atk, def, exp,level);
 		}
 		else
 		{
@@ -44,7 +53,7 @@ public class nothing
 		}
 
 	}
-	public static void hunt(String user_name, int hp, int atk, int def, int exp)
+	public static void hunt(String user_name, int hp, int atk, int def, int exp, int level)
 	{
 		Scanner up = new Scanner(System.in);
 		nothing you = new nothing();
@@ -55,68 +64,54 @@ public class nothing
 		String b = "hp";
 		String c = "def";
 		String d = "exp";
+		
 		int mon_hp =  rand.nextInt(60) + 40;
 		int mon_atk = rand.nextInt(24) + 12;
 		int mon_def = rand.nextInt(30) + 15;
+		
+	
 		
 		System.out.println("!!Monster Appeared!!");
 		System.out.printf("----------------" + "%2S" + "-------------\n", mon_name);
 		System.out.printf("|" + "%8S" + " = " + "%5s" + "%8S" + " = " + "%6s" + "|\n", b, mon_hp, a, mon_atk);
 		System.out.printf("|" + "%8S" + " = " + "%5s" + "                 |\n", c, mon_def);
 		
-<<<<<<< HEAD
-		you.askHunt(a,b,c,d,hp,atk,def,exp,mon_atk,user_name,mon_def,mon_hp,mon_name);
+		you.askHunt(a,b,c,d,hp,atk,def,exp,mon_atk,user_name,mon_def,mon_hp,mon_name, level);
 	}
-	public static void askHunt(String a, String b, String c, String d,  int hp, int atk, int def,int exp, int mon_atk,String user_name,  int mon_def, int mon_hp, String mon_name)
-=======
-		you.askHunt(a,b,c,d,atk,def,mon_atk,hp,user_name,exp,mon_def,mon_hp,mon_name);
-	}
-	public static void askHunt(String a, String b, String c, String d, int atk, int def, int mon_atk, int hp, String user_name, int exp, int mon_def, int mon_hp, String mon_name);
->>>>>>> origin/master
+	public static void askHunt(String a, String b, String c, String d,  int hp, int atk, int def,int exp, int mon_atk,String user_name,  int mon_def, int mon_hp, String mon_name, int level)
 	{
 		Scanner up = new Scanner(System.in);
 		nothing you = new nothing();
 		Random rand = new Random();
-<<<<<<< HEAD
 
-=======
-		
-		String mon_name = "Erhdas";
-		String a = "atk";
-		String b = "hp";
-		String c = "def";
-		String d = "exp";
-		int mon_hp =  rand.nextInt(60) + 40;
-		int mon_atk = rand.nextInt(24) + 12;
-		int mon_def = rand.nextInt(30) + 15;
->>>>>>> origin/master
 		System.out.print("What would you do? [Attack Flee Defend]\n");
 		String user_command = up.nextLine();
 		
 		if(user_command.equals("attack")||user_command.equals("Attack"))
 		{
-			you.fight(a,b,c,d,atk,def,mon_atk,hp,user_name,exp,mon_def,mon_hp,mon_name);
+			you.fight(a,b,c,d,atk,def,mon_atk,hp,user_name,exp,mon_def,mon_hp,mon_name,level);
 		}
 		else if(user_command.equals("Flee")||user_command.equals("flee"))
 		{
-			you.stat(user_name, hp, atk, def, exp);
+			you.stat(user_name, hp, atk, def, exp, level);
 		}
 		else if(user_command.equals("defend")||user_command.equals("Defend"))
 		{
-<<<<<<< HEAD
-			you.defense(a,b,c,d,atk,mon_def,mon_hp,mon_atk, mon_name, def, exp, user_name, hp);
-=======
-			you.defense(a,b,c,atk,mon_def,mon_hp,mon_atk, mon_name, def, exp, user_name, hp);
->>>>>>> origin/master
+			you.defense(a,b,c,d,atk,mon_def,mon_hp,mon_atk, mon_name, def, exp, user_name, hp, level);
+		}
+		else
+		{
+			you.askHunt(a,b,c,d,hp,atk,def,exp,mon_atk,user_name,mon_def,mon_hp,mon_name, level);
 		}
 	}
-	public static void fight(String a, String b, String c, String d, int atk, int def, int mon_atk, int hp, String user_name, int exp, int mon_def, int mon_hp, String mon_name)
+	public static void fight(String a, String b, String c, String d, int atk, int def, int mon_atk, int hp, String user_name, int exp, int mon_def, int mon_hp, String mon_name, int level)
 	{
 		nothing you = new nothing();
 		Random rand = new Random();
 		
+		hp = hp - mon_atk;
+			
 		int actual_a = atk - mon_def;
-<<<<<<< HEAD
 		mon_hp = mon_hp - actual_a;
 		if(mon_hp <= 0){
 			mon_hp = 0;
@@ -126,42 +121,21 @@ public class nothing
 		System.out.printf("|" + "%8S" + " = " + "%5s" + "%8S" + " = " + "%6s" + "|\n", b, mon_hp, a, mon_atk);
 		System.out.printf("|" + "%8S" + " = " + "%5s" + "                 |\n", c, mon_def);
 		System.out.println("\"Erhdas\" got damaged by " + actual_a);
+		System.out.println("\"Erdas\" used \'Fireball\'!\nYou got burned by " + mon_atk);
 		
 		if(mon_hp <= 0){
-=======
-		int d_mon_hp = mon_hp - actual_a;
-		if(d_mon_hp <= 0){
-			d_mon_hp = 0;
-		}
-		
-		System.out.printf("----------------" + "%2S" + "-------------\n", mon_name);
-		System.out.printf("|" + "%8S" + " = " + "%5s" + "%8S" + " = " + "%6s" + "|\n", b, d_mon_hp, a, mon_atk);
-		System.out.printf("|" + "%8S" + " = " + "%5s" + "                 |\n", c, mon_def);
-		System.out.println("\"Erhdas\" got damaged by " + actual_a);
-		
-		if(d_mon_hp <= 0){
->>>>>>> origin/master
 			exp = rand.nextInt(100) + 75;
 			System.out.println("!!" + mon_name + " died!!\nYou've earned " + exp + " points!!");
-			you.stat(user_name, atk, def, exp, hp);
+			you.stat(user_name, hp, atk, def, exp, level);
 		}
-<<<<<<< HEAD
 		else if(mon_hp > 0){
-			you.askHunt(a,b,c,d,hp,atk,def,exp,mon_atk,user_name,mon_def,mon_hp,mon_name );
+			you.askHunt(a,b,c,d,hp,atk,def,exp,mon_atk,user_name,mon_def,mon_hp,mon_name,level);
 		}
 	}
-	public static void defense(String a, String b, String c, String d, int atk, int mon_def, int mon_atk, int mon_hp, String mon_name, int def, int exp, String user_name, int hp)
+	public static void defense(String a, String b, String c, String d, int atk, int mon_def, int mon_atk, int mon_hp, String mon_name, int def, int exp, String user_name, int hp, int level)
 	{
 		nothing you = new nothing();
 		
-=======
-		else if(d_mon_hp > 0){
-			you.askHunt(a,b,c,atk,mon_def,mon_hp,mon_atk, mon_name, def, exp, user_name, hp);
-		}
-	}
-	public static void defense(String a, String b, String c, int atk, int mon_def, int mon_atk, int mon_hp, String mon_name, int def, int exp, String user_name, int hp)
-	{
->>>>>>> origin/master
 		int actual_d = mon_atk - def;
 		if(actual_d < 0){
 			actual_d = 0;
@@ -173,10 +147,7 @@ public class nothing
 		System.out.printf("|" + "%8S" + " = " + "%5s" + "%8S" + " : " + "%6s" + "|\n", c, def, d, exp);
 		
 		System.out.println(user_name + " got damaged by " + actual_d);
-<<<<<<< HEAD
 		
-		you.askHunt(a,b,c,d,hp,atk,def,exp,mon_atk,user_name,mon_def,mon_hp,mon_name );
-=======
->>>>>>> origin/master
+		you.askHunt(a,b,c,d,hp,atk,def,exp,mon_atk,user_name,mon_def,mon_hp,mon_name,level);
 	}
 }
