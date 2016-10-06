59592a28589cf3f8651897cfbc9dbcfbf2e8b6e2
San234 <allen525@naver.com>
2016-10-06 12:13:45 -0700

    nothing

--- nothing.java
+++ nothing.java
@@ -25,7 +25,7 @@ public class nothing
 	{
 		Scanner up = new Scanner(System.in);
 		nothing you = new nothing();
-		
+		Random rand = new Random();
 		String a = "atk";
 		String b = "hp";
 		String c = "def";
@@ -39,6 +39,8 @@ public class nothing
 			exp = exp - 200;
 		}
 	
+		int ErhdasEnc = rand.nextInt(2)+1;
+	
 		System.out.printf("-----------------" + "%2S" + "-------------\n", user_name);
 		System.out.printf("|" + "%8S" + " = " + "%5s" + "%8S" + " = " + "%6s" + "|\n", b, hp, a, atk);
 		System.out.printf("|" + "%8S" + " = " + "%5s" + "%8S" + " : " + "%6s" + "|\n", c, def, d, exp);
@@ -47,7 +49,12 @@ public class nothing
 		String hunt_ask = up.nextLine();
 		if(hunt_ask.equals("y") || hunt_ask.equals("yes"))
 		{
-			you.hunt(user_name, hp, atk, def, exp,level);
+			if (ErhdasEnc == 1){
+			you.hunt1(user_name, hp, atk, def, exp,level);
+			}
+			else if(ErhdasEnc == 2){
+				you.hunt2(user_name, hp, atk, def, exp,level);
+			}
 		}
 		else
 		{
@@ -56,7 +63,7 @@ public class nothing
 		}
 
 	}
-	public static void hunt(String user_name, int hp, int atk, int def, int exp, int level)
+	public static void hunt1(String user_name, int hp, int atk, int def, int exp, int level)
 	{
 		Scanner up = new Scanner(System.in);
 		nothing you = new nothing();
@@ -104,6 +111,58 @@ public class nothing
 		
 		you.askHunt(a,b,c,d,hp,atk,def,exp,mon_atk,user_name,mon_def,mon_hp,mon_name, level);
 	}
+	public static void hunt2(String user_name, int hp, int atk, int def, int exp, int level)
+	{
+		Scanner up = new Scanner(System.in);
+		nothing you = new nothing();
+		Random rand = new Random();
+		
+		String mon_name = "Erhdas";
+		String a = "atk";
+		String b = "hp";
+		String c = "def";
+		String d = "exp";
+		
+		String mon_kind1 = "of Happy";
+		String mon_kind2 = "of Fury";
+		String mon_kind3 = "of Sad";
+		String mon_kind4 = "of Enjoy";
+		
+		int mon_hp =  rand.nextInt(60) + 40;
+		int mon_atk = rand.nextInt(48) + 28;
+		int mon_def = rand.nextInt(12) + 6;
+		
+		System.out.println("!!Monster Appeared!!");
+		System.out.println("				|#|                               |#|   |#|");
+		System.out.println("				|##|                              |##| |#|");
+		System.out.println("				 |##|							   |#||#|");
+		System.out.println("				 |#|							    |##|");
+		System.out.println("				  |#|							   |#|");
+		System.out.println("				   |##|	  @@@@@@@@@@@@@@@@@@@@@	 |##|");
+		System.out.println("				     |#|@@@@@@@@@@@@@@@@@@@@@@@@@@@");																	
+		System.out.println("	 				  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+		System.out.println("	                 @@@@@@@@0000000000@@@@@@@@@@@@@@@");
+		System.out.println("                    @@@@@@@000%%%%%%%%%000@@@@@@@@@@@@@@");
+		System.out.println("                    @@@@@@00%%%%%%%%%%%%%000@@@@@@@@@@@@");
+		System.out.println("                    @@@@@@00%%%%%%%%%%%%%000@@@@@@@@@@@@@");
+		System.out.println("                @@@@@@@@@@00%%%%%%%%%%%%%000@@@@@@@@@@@@@@@@@");
+		System.out.println("              &@@@@@@@@@@@00%%%%%%%%%%%%%000@@@@@@@@@@@@@@@@@@&");
+		System.out.println("            &&@@@@@@@@@@@@@00%%%%%%%%%%%000@@@@@@@@@@@@@@@@@@@@&&");
+		System.out.println("           &&&&&@@@@@@@@@@@@0000000000000@@@@@@@@@@@@@@@@@@@@@&&&&");
+		System.out.println("           &&&&&&&&|@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&&&&&&&&");
+		System.out.println("           &&&&&&&&|@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@|&&&&&&&&&&&&");
+		System.out.println("           &&&&&&&&|@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@|&&&&&&&&&&&&");
+		System.out.println("           &&&&&&&&|@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@|&&&&&&&&&&&&");
+		System.out.println("           &&&&&&&&|@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@|&&&&&&&&&&&&");
+		System.out.println("           &&&&&&&&|@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@|&&&&&&&&&&&&");
+		System.out.println("           &&&&&&&&|@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@|&&&&&&&&&&&&");
+		
+		System.out.printf("|" + "%16S" + " %-16s" + "|\n", mon_name, mon_kind2);
+		System.out.printf("|" + "%8S" + " = " + "%5s" + "%8S" + " = " + "%6s" + "|\n", b, mon_hp, a, mon_atk);
+		System.out.printf("|" + "%8S" + " = " + "%5s" + "                 |\n", c, mon_def);
+		
+		you.askHunt(a,b,c,d,hp,atk,def,exp,mon_atk,user_name,mon_def,mon_hp,mon_name, level);
+	}
 	public static void askHunt(String a, String b, String c, String d,  int hp, int atk, int def,int exp, int mon_atk,String user_name,  int mon_def, int mon_hp, String mon_name, int level)
 	{
 		Scanner up = new Scanner(System.in);
@@ -115,7 +174,7 @@ public class nothing
 		
 		if(user_command.equals("attack")||user_command.equals("Attack"))
 		{
-			you.fight(a,b,c,d,atk,def,mon_atk,hp,user_name,exp,mon_def,mon_hp,mon_name,level);
+			you.fight1(a,b,c,d,atk,def,mon_atk,hp,user_name,exp,mon_def,mon_hp,mon_name,level);
 		}
 		else if(user_command.equals("Flee")||user_command.equals("flee"))
 		{
@@ -130,24 +189,25 @@ public class nothing
 			you.askHunt(a,b,c,d,hp,atk,def,exp,mon_atk,user_name,mon_def,mon_hp,mon_name, level);
 		}
 	}
-	public static void fight(String a, String b, String c, String d, int atk, int def, int mon_atk, int hp, String user_name, int exp, int mon_def, int mon_hp, String mon_name, int level)
+	public static void fight1(String a, String b, String c, String d, int atk, int def, int mon_atk, int hp, String user_name, int exp, int mon_def, int mon_hp, String mon_name, int level)
 	{
 		nothing you = new nothing();
 		Random rand = new Random();
 		
 		hp = hp - mon_atk;
 			
+		String mon_kind1 = "of Happy";
 		int actual_a = atk - mon_def;
 		mon_hp = mon_hp - actual_a;
 		if(mon_hp <= 0){
 			mon_hp = 0;
 		}
 		
-		System.out.printf("|" + "%16S" + " %-16s" + "|\n", mon_name, mon_kind1);
-		System.out.printf("|" + "%8S" + " = " + "%5s" + "%8S" + " = " + "%6s" + "|\n", b, mon_hp, a, mon_atk);
-		System.out.printf("|" + "%8S" + " = " + "%5s" + "                 |\n", c, mon_def);
+		System.out.printf("|%16S %-16s|\n", mon_name, mon_kind1);
+		System.out.printf("|%8S = %5s%8S = %6s|\n", b, mon_hp, a, mon_atk);
+		System.out.printf("|%8S = %5s                 |\n", c, mon_def);
 		System.out.println("\"Erhdas\" got damaged by " + actual_a);
-		System.out.println("\"Erdas\" used \'Fireball\'!\nYou got burned by " + mon_atk);	
+		System.out.println("\"Erdhas\" used \'Fireball\'!\nYou got burned by " + mon_atk);	
 		
 		if(hp <= 0){
 			hp = 0;
@@ -173,10 +233,10 @@ public class nothing
 		if(actual_d < 0){
 			actual_d = 0;
 		}
-		int d_hp = hp - actual_d;
+		hp = hp - actual_d;
 		
 		System.out.printf("-----------------" + "%2S" + "-------------\n", user_name);
-		System.out.printf("|" + "%8S" + " = " + "%5s" + "%8S" + " = " + "%6s" + "|\n", b, d_hp, a, atk);
+		System.out.printf("|" + "%8S" + " = " + "%5s" + "%8S" + " = " + "%6s" + "|\n", b, hp, a, atk);
 		System.out.printf("|" + "%8S" + " = " + "%5s" + "%8S" + " : " + "%6s" + "|\n", c, def, d, exp);
 		
 		System.out.println(user_name + " got damaged by " + actual_d);
