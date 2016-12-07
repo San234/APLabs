5e1be4ffdc2f00560895708c404069f4d843b823
San234 <allen525@naver.com>
2016-12-07 10:44:06 -0800

    Study Guide #4

---
+++ Study Guides/Study Guide #4/SG4_1.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class SG4_1
+{
+	static int i;
+	public static void main(String[]args)
+	{
+		int[][]nums = new int[2][2];
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		
+		System.out.println("\n\n\tC C 5 A D\"2 4\" A A B C B D\t\n");
+
+		int j =doSomething();
+		
+		System.out.print(j);
+	}
+	public static int doSomething(){
+		for(i = 0; i <5;i++){
+			i = i;
+		}
+		return i;
+	}
+}
