dbdc3c7f0995169afa5809bee70d21c36c6d11f4
San234 <allen525@naver.com>
2017-03-20 12:08:15 -0700

    Dash C# Script

---
+++ Dash.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dash : MonoBehaviour {
+
+	public Vector3 moveDirection;
+	public float MaxDashTime = 1.0f;
+	public float dashSpeed = 1.0f;
+	public float dashStoppingSpeed = 0.1f;
+
+	private float currentDashTime;
+
+	void Start()
+	{
+		currentDashTime = MaxDashTime;
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown("space"))
+		{
+			currentDashTime = 0.0f;
+		}
+		if (currentDashTime < MaxDashTime)
+		{
+			moveDirection = new Vector3(0, 0, dashSpeed);
+			currentDashTime += dashStoppingSpeed;
+		}
+		else
+		{
+			moveDirection = Vector3.zero;
+		}
+		transform.Translate(moveDirection*Time.deltaTime);
+	}
+}
